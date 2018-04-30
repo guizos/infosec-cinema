9fb035b1d27efbb8ede86da2d7ffe97d12751bbe
Jorge Blasco Alís <Jorge.BlascoAlis@rhul.ac.uk>
2018-04-30 23:59:41 +0200

    start loading files

--- js/infosec-cinema.js
+++ js/infosec-cinema.js
@@ -1,8 +1,9 @@
 
 
-var remoteCouch = false;
-
-
+$(function() {
+    console.log( "ready!" );
+    addAllMovies();
+});
 
 function addMovie(file) {
     const movie_json = JSON.parse(file);
@@ -20,30 +21,15 @@ function addMovie(file) {
   }
 
   function addAllMovies() {
-    var file = 'list.json'
-    fs.readFile(file, (err, data) => {
-        if (err && err.code === "ENOENT") {
-            // But the file might not yet exist.  If so, just write the object and bail
-            return fs.writeFile(file, JSON.stringify([obj]), error => console.error);
-        } else if (err) {
-            // Some other error
-            console.error(err);
-        }
-        // 2. Otherwise, get its JSON content
-        else {
-            try {
-                const movie_list = JSON.parse(data);
-
-                //load data for each movie
-                var arrayLength = movie_list.length;
-                for (var i = 0; i < arrayLength; i++) {
-                    console.log(movie_list[i]);
-                    //Do something
-                    //addMovie(movie_list[i]);
-                }
-            } catch (exception) {
-                console.error(exception);
-            }
+    const file = 'movies/list.json'
+    $.getJSON(file, function(json) {
+        console.log(json); // this will show the info it in firebug console
+        //load data for each movie
+        var arrayLength = movie_list.length;
+        for (var i = 0; i < arrayLength; i++) {
+            console.log(movie_list[i]);
+            //Do something
+            //addMovie(movie_list[i]);
         }
     });
   }
